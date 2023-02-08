97bf9da4409f8a152396c444f47509b02f720a3a
jordy <jordy.roumans@hotmail.com>
2023-02-08 14:17:08 +0100

    Initial push, models

---
+++ server/src/TickTick/TickTick.Models/base/Location.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TickTick.Models
+{
+    public enum LocationType
+    {
+        PRIMARY=0,
+        WORK=1,
+        HOME=2,
+        OTHER=3
+    }
+    public sealed class Location: BaseEntity
+    {
+        public string Street { get; set; }
+        public string Nr { get; set; }
+        public string City { get; set; }
+        public string? State { get; set; }
+        public string ZipCode { get; set; }
+        public string Country { get; set; }
+        public LocationType Type { get; set; } = LocationType.PRIMARY;
+
+        public Location(string street, string nr, string city, string zipcode, string country) 
+        {
+            this.Street = street;
+            this.Nr = nr;
+            this.City = city;
+            this.ZipCode = zipcode;
+            this.Country = country;
+
+        }
+
+        public Location(string street, string nr, string city, string zipcode, string country,string state):this(street, nr, city, zipcode, country)
+        {
+            this.State = state;
+
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Street} {this.Nr}, {this.City}";
+        }
+    }
+}
---
+++ server/src/TickTick/TickTick.Models/base/Person.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TickTick.Models
+{
+    public class Person :BaseAuditableEntity, IEquatable<Person>
+    {
+        public  Guid PublicId { get; private set; }
+        public string FirstName { get; set; }
+        public string? MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string? SocialSecurityNumber { get; set; }
+        public DateTime? DateOfBirth  { get; set; }
+        public string Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public IList<Location>? Address { get; set; }
+        public bool IsDeleted { get; private set; }
+
+
+        public Person(string firstname, string lastname, string email)
+        {
+            this.FirstName= firstname;
+            this.LastName= lastname;
+            this.Email= email;
+        }
+
+        public void Delete()
+        {
+            this.IsDeleted= true;
+        }
+        public void UnDelete()
+        {
+            this.IsDeleted= false; 
+        }
+
+        public void Update()
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName}";
+        }
+
+        public bool Equals(Person? other)
+        {
+            if (string.IsNullOrEmpty(this.SocialSecurityNumber) && !string.IsNullOrEmpty(other?.SocialSecurityNumber))
+            {
+                return this.SocialSecurityNumber == other.SocialSecurityNumber;
+            }
+            else
+            {
+                return this.PublicId == other?.PublicId;
+            }
+        }
+    }
+}
