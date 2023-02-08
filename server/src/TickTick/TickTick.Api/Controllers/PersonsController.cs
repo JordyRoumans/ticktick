8e1b5c862b24282ecbc1a762a55299879b90ae8d
jordy <jordy.roumans@hotmail.com>
2023-02-08 14:45:59 +0100

    people

---
+++ server/src/TickTick/TickTick.Api/Controllers/PersonsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TickTick.Models;
+
+namespace TickTick.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            List<Person> people = new List<Person>();
+            people.Add(new Person("Kevin", "DeRudder", "kevin.derudder@elmos.be"));
+            people.Add(new Person("Jordy", "Schuermans", "jordy.schuermans@assign.be"));
+            return Ok(people);
+
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(Guid id) 
+        {
+            //TODO: haal persoon op
+            Person person = new Person("Jordy", "Schuermans", "jordy.schuermans@assign.be");
+            return Ok(person);
+        }
+    }
+}
+ 
